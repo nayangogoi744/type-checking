399faf6ed78791304153934956a4665cc048dacc
Jyoti Gogoi <njy@softwareag.com>
2020-04-01 16:46:32 +0530

    added all the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Pt from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    render(){
+    return(
+        <div>
+    <h1>
+        Hello {this.props.name}
+    </h1>
+    <h2>
+        Roll  {this.props.roll}
+    </h2></div>
+    );
+   };
 }
 
+
+App.propType={
+    name : Pt.string,
+    roll : Pt.number
+};
 export default App;
